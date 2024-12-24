edd288e350a45e60b429985596029d6c293c6d2b
aslan-katsiev <katsiev.001@mail.ru>
2024-12-24 15:20:18 +0300

    add objectives

--- main.py
+++ main.py
@@ -1,21 +1,6 @@
-import sys
-import random
-from PyQt6.QtWidgets import (
-    QApplication,
-    QMainWindow,
-    QPushButton,
-    QGridLayout,
-    QWidget,
-    QInputDialog,
-    QMessageBox,
-)
-from PyQt6.QtCore import Qt  # Импортируем Qt
-
-
-class GameLogic:
-    def __init__(self, rows, cols, mines):
-        self.rows = rows
-        self.cols = cols
-        self.mines = mines
-        self.board = [[0 for _ in range(cols)] for _ in range(rows)]
-        self.generate_mines()
+# 1 Создать игровое поле, а именно двумерный массив заполненный нулями
+# 2 Создать реакцию на первое нажатие пользователя: открытие клеток вокруг первой клетки и более, генерация мин
+# 3 Создать механику показа бомб вокруг клетки
+# 4 Создать функцию открытия клетки и поставить флажок
+# 5 Создать интерфейс - главное меню (выбор сложности) и главное поле
+# 6 Создать реакцию на победу или поражение пользователя
